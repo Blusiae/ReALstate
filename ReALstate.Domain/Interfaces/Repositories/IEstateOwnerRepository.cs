3bbc926e319aa37a436c625c28b8c116c9aeb067
JaKuba319 <kuba.trembaczowski@gmail.com>
2024-06-02 14:52:15 +0200

    created-repositories-for-estate-and-owner

---
+++ ReALstate.Domain/Interfaces/Repositories/IEstateOwnerRepository.cs
@@ -0,0 +1,13 @@
+﻿using ReALstate.Domain.Entities;
+
+namespace ReALstate.Domain.Interfaces.Repositories
+{
+    public interface IEstateOwnerRepository
+    {
+        Task<IEnumerable<EstateOwner>> GetAllAsync();
+        Task<EstateOwner?> GetByIdAsync(int id);
+        Task<int> Create(EstateOwner owner);
+        Task Delete(EstateOwner owner);
+        Task SaveChanges();
+    }
+}
