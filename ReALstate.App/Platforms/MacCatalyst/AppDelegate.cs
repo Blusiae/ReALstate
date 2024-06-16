5d9698206215267fa5c7549347c43114f2129ade
Daniel Kocot <42910231+Blusiae@users.noreply.github.com>
2024-06-16 17:40:15 +0200

    create dashboard page

--- ReALstate.App/Platforms/MacCatalyst/AppDelegate.cs
+++ ReALstate.App/Platforms/MacCatalyst/AppDelegate.cs
@@ -1,6 +1,6 @@
 ﻿using Foundation;
 
-namespace ReALstate.App
+namespace ReALstate.App.Platforms.MacCatalyst
 {
     [Register("AppDelegate")]
     public class AppDelegate : MauiUIApplicationDelegate
