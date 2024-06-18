8880dfddfd69c5f99db466744626f9ce455dd138
JaKuba319 <kuba.trembaczowski@gmail.com>
2024-06-18 22:06:23 +0200

    added-repository

---
+++ ReALstate.Domain/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using ReALstate.Domain.Entities;
+
+namespace ReALstate.Domain.Interfaces.Repositories
+{
+    public interface IUserRepository 
+    {
+        Task<IEnumerable<User>> GetAllAsync();
+        Task<User?> GetByIdAsync(Guid id);
+        Task<Guid> Create(User entity);
+        Task Delete(User entity);
+    }
+}
